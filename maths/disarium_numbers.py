0d19da4dc159c50634eb4886e418a46d4fe6785b
Sattwik Sahu <sahusattwik@gmail.com>
2020-12-14 14:55:31 +0530

    Added link and tests for 0 and negative numbers

--- maths/disarium_numbers.py
+++ maths/disarium_numbers.py
@@ -1,10 +1,12 @@
 def is_disarium(num: int) -> bool:
     """
     Checks if a number is a "Disarium number".
-    A Disarium number is a number defined by
-    the following process: Sum of its digits
-    powered with their respective position is
-    equal to the original number.
+    A Disarium number is a number defined by the following process:
+    Sum of its digits powered with their respective position is equal
+    to the original number. The number must be a whole number greater than 0.
+
+    For more info on disarium numbers:
+    https://www.geeksforgeeks.org/disarium-number/
 
     >>> is_disarium(89)
     True
@@ -18,5 +20,12 @@ def is_disarium(num: int) -> bool:
     False
     >>> is_disarium(20)
     False
+    >>> is_disarium(0)
+    False
+    >>> is_disarium(-3)
+    False
     """
-    return sum([int(digit) ** (pos + 1) for pos, digit in enumerate(str(num))]) == num
+    if (num >= 0):
+        return sum([int(digit) ** (pos + 1) for pos, digit in enumerate(str(num))]) == num
+    else:
+        return False
